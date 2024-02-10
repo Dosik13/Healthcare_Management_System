39d06f4e49250c1a7f90c4e62da7ea3a8f8b1905
Dosi Kolev <dosi@uni-sofia.bg>
2024-02-10 02:43:32 +0200

    before updating all models

--- app/models/adminstrator.go
+++ app/models/adminstrator.go
@@ -1,10 +1,6 @@
 package models
 
 type Administrator struct {
-	ID        uint   `gorm:"primaryKey"`
-	UserID    string `gorm:"uniqueIndex;type:varchar(255);not null"`
-	Password  string `gorm:"type:varchar(255);not null"`
-	FirstName string `gorm:"type:varchar(255);not null"`
-	LastName  string `gorm:"type:varchar(255);not null"`
-	Email     string `gorm:"uniqueIndex;type:varchar(255);not null"`
+	User
+	Role string `gorm:"type:varchar(255);not null"`
 }
--- app/models/user.go
+++ app/models/user.go
@@ -3,20 +3,16 @@ package models
 import "time"
 
 type User struct {
-	ID          uint      `gorm:"primaryKey"`
-	UserID      string    `gorm:"uniqueIndex;type:varchar(255);not null"`
-	Password    uint      `gorm:"type:varchar(255);not null"`
+	UserID      uint      `gorm:"primaryKey"`
 	FirstName   string    `gorm:"type:varchar(255);not null"`
 	MiddleName  string    `gorm:"type:varchar(255);not null"`
 	LastName    string    `gorm:"type:varchar(255);not null"`
 	Email       string    `gorm:"uniqueIndex;type:varchar(255);not null"`
+	Password    uint      `gorm:"type:varchar(255);not null"`
 	DateOfBirth time.Time `gorm:"not null"`
-	Age         int       `gorm:"not null"`
 	Address     string    `gorm:"type:varchar(255);not null"`
 	PhoneNumber string    `gorm:"type:varchar(255);not null"`
 	Gender      string    `gorm:"type:varchar(255);not null"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
-
-	/**/
 }
